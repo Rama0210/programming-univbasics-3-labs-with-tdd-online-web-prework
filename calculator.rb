90c8b945cb91f2662ada1dca7ecaf190289d942d
upbeat-torrent-2014 <ramaabrahams@yahoo.com>
2019-08-05 01:39:40 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -4,5 +4,3 @@ sum=first_number+second_number
 difference=first_number-second_number
 product=first_number*second_number
 quotient=first_number/second_number
-=======
->>>>>>> 16544fbf3530c20aa53028e0a002f83482096b91
